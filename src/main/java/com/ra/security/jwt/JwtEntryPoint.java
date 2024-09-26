39076624f6b8ad8061008ed9ffc9ca1bf937c0ee
your new username <smallmonster900@gmail.com>
2024-09-26 21:29:39 +0700

    add componet jwtEntryPoint

--- src/main/java/com/ra/security/jwt/JwtEntryPoint.java
+++ src/main/java/com/ra/security/jwt/JwtEntryPoint.java
@@ -8,9 +8,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-
+@Component
 public class JwtEntryPoint implements AuthenticationEntryPoint {
     private Logger logger = LoggerFactory.getLogger(JwtEntryPoint.class);
     @Override
